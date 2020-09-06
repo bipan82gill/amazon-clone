40eaddf1ec78602f0c762753196a6061a866dc05
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-09-06 15:25:56 -0400

    create product details page

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import data from '../data';
 
 function ProductScreen(props) {
     console.log(props.match.params.id)
     const product = data.products.find(x=> x._id === props.match.params.id)
-    return <div>{product.name} </div>
+    return <div>
+                <div className="back-to-result">
+                    <Link to ="/">Back to result</Link>
+                </div>
+                <div className ="details">
+                    <div className ="details-image">
+                        <img src={product.image} alt ="product"/>
+                    </div>
+                    <div className ="details-info">
+                        <ul>
+                            <li>
+                               <h4> {product.name}</h4>
+                            </li>
+                            <li>
+                                {product.rating} Stars ({product.numReviews})
+                            </li>
+                            <li>
+                               <b> Price: ${product.price}</b>
+                            </li>
+                            <li>
+                               Description {product.description}
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="details-action">
+                        <ul>
+                            <li>
+                                Price: ${product.price}
+                            </li>
+                            <li>
+                                status: {product.status}
+                            </li>
+                            <li>
+                                Qty: <select>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </select>
+                            </li>
+                            <li>
+                            <button className ="button">Add to cart</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                
+         </div>
 }
 export default ProductScreen;

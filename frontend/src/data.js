b102e28012a2d20ac5c2825359b59f6b882e3dc3
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-09-05 22:09:35 -0400

    adding products array

---
+++ frontend/src/data.js
@@ -0,0 +1,39 @@
+export default  {
+    products :[{
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image:'/images/slim-1.jpg',
+        price: 60,
+        brand:'Nike',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image:'/images/slim-1.jpg',
+        price: 60,
+        brand:'Nike',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image:'/images/slim-1.jpg',
+        price: 60,
+        brand:'Nike',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image:'/images/slim-1.jpg',
+        price: 60,
+        brand:'Nike',
+        rating: 4.5,
+        numReviews:10
+    }
+]
+}

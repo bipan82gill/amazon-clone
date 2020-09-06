77ea841240a7244be9c1d90e587419dde4ff7564
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-09-06 16:55:45 -0400

    create server

---
+++ backend/data.js
@@ -0,0 +1,43 @@
+export default  {
+    products :[{
+        _id: '123',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image:'/images/slim-1.jpg',
+        price: 60,
+        brand:'Nike',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        _id: '124',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image:'/images/slim-1.jpg',
+        price: 60,
+        brand:'Nike',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        _id: '125',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image:'/images/slim-1.jpg',
+        price: 60,
+        brand:'Nike',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        _id: '126',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image:'/images/slim-1.jpg',
+        price: 60,
+        brand:'Nike',
+        rating: 4.5,
+        numReviews:10
+    }
+]
+}

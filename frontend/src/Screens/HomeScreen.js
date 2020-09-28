f63241a416101b9fa7d8f815ef10602df2d60a5b
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-09-27 20:06:10 -0400

    add dispatcher

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { listProducts } from '../actions/productActions';
+
 
 function HomeScreen(props) {
-  const [products, setProduct] = useState([]);
 
+  const productList = useSelector(state=> state.productList);
+  const { products, loading, error } = productList;
+  const dispatch = useDispatch();
   useEffect(()=> {
-    const fetchData = async () => {
-      const { data } = await axios.get("/api/products");
-      setProduct(data);
-    }
-    fetchData();
+   dispatch(listProducts());
      return() => {
 
      }
   }, [])
 
-  return (
+  return (    
+  loading? <div>Loading ...</div>:
+  error? <div>{error}</div>:
     <ul className ="products">
-        { 
-              products.map(product => 
+        { products.map(product => 
                 <li key ={product._id}>
                   <div className="product">
                   <Link to={"/product/"+product._id}>                  
@@ -36,6 +38,7 @@ function HomeScreen(props) {
                 </li>)
             }  
             </ul>
-    )
+  )
+    
 }
 export default HomeScreen;

7701b9c79924ca01250483ea5d78dabee3977c06
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-10-11 15:25:07 -0400

    creating routes for user admin

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,19 @@ import data from './data';
 import config from './config';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import userRoute from './routes/userRoute';
 
 dotenv.config();
 
 const mongodbUrl = config.MONGODB_URL;
 mongoose.connect(mongodbUrl,{
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    userUnifiedTopology: true,
+    userCreateIndex: true
 }).catch(error => console.log(error.reason));
 
 const app = express();
-
+app.use("/api/users", userRoute);
 app.get("/api/products/:id", (req, res)=>{
     const productId = req.params.id;
     res.send(data.products.find(x=>x._id === productId));
